e931e38695b52f71351dee4b15c4313db6c875f8
max kolomeychenko <maxim.kolomeychenko@gmail.com>
2021-09-28 14:41:36 +0300

    update strucutre

--- main.py
+++ main.py
@@ -3,70 +3,61 @@ import supervisely_lib as sly
 import cv2
 import copy
 
-# Init API variables
-address = 'https://app.supervise.ly/'
-
-token = ''  # paste from https://app.supervise.ly/user/settings/tokens
-
 # Initialize API object
+address = 'http://supervisely.private:38585/'
+token = 'gvmiNcW8RR1LpatrVaIwrv9mtICOHBS7yhxpaKn70RsCDJZJVk6HpPxtMNhsFcRKkGe7t7DPQueVMG7445FSUUvkuAyAKSEbC3DSh2ek10nv6zJfYfM2NnzV5WMN8EKi'
 api = sly.Api(address, token)
 
 
 """
-STEP 1 — Creating and uploading project
+STEP 1 — Create project
 """
 
-
-# Initialize annotation(-s)
-class_defect = sly.ObjClass('defect', sly.Rectangle)
-
-label_defect = sly.Label(sly.Rectangle(top=46, left=122, bottom=114, right=180), class_defect)
-
-test_image = cv2.imread('./test_image.png')
-height, width = test_image.shape[0], test_image.shape[1]
-
-defect_annotation = sly.Annotation((height, width), [label_defect])
-
-
 # Init project remotely
-workspace_id = 60361
+workspace_id = 354
 
 project_name = 'demo_project'
 dataset_name = 'demo_dataset'
 
-project = api.project.create(workspace_id, project_name, type=sly.ProjectType.IMAGES,
-                             change_name_if_conflict=True)
-dataset = api.dataset.create(project.id, f'{dataset_name}',
-                             change_name_if_conflict=True)
+project = api.project.create(workspace_id, project_name, type=sly.ProjectType.IMAGES, change_name_if_conflict=True)
+dataset = api.dataset.create(project.id, f'{dataset_name}', change_name_if_conflict=True)
 
 
-# Updating meta
-objects = sly.ObjClassCollection([class_defect])
-project_meta = sly.ProjectMeta(obj_classes=objects, project_type='images')
-
+# Update project classes
+class_defect = sly.ObjClass('defect', sly.Rectangle)
+classes = sly.ObjClassCollection([class_defect])
+project_meta = sly.ProjectMeta(classes)
 updated_meta = api.project.update_meta(project.id, project_meta.to_json())
 
+"""
+STEP 1 — Uploade img+ann
+"""
 
-# Uploading image
-img_info = api.image.upload_path(dataset.id, name="test_image.png", path="./test_image.png")
+# Uploade image
+test_image = sly.image.read('./test_image.png') # RGB
+height, width = test_image.shape[0], test_image.shape[1]
+img_info = api.image.upload_np(dataset.id, name="test_image.png", img=test_image)
 
-# Uploading annotation
-uploaded_annotation_json = api.annotation.upload_json(img_info.id, defect_annotation.to_json())
+# Uploade annotation
+label_defect = sly.Label(sly.Rectangle(top=46, left=122, bottom=114, right=180), class_defect)
+ann = sly.Annotation((height, width), [label_defect])
+
+api.annotation.upload_ann(img_info.id, ann)
 
 
 """
-STEP 2 — Downloading and visualizing results
+STEP 2 — Downloade and visualize results
 """
 
-# Downloading image
+# Downloade image
 ann_info = api.annotation.download(img_info.id)
 ann = sly.Annotation.from_json(ann_info.annotation, project_meta)
 
-# Downloading annotations
+# Downloade annotations
 img = api.image.download_np(img_info.id)
 new_img = copy.deepcopy(img)
 
-# Draw Annotation on image before crop
+# Draw Annotation on image
 ann.draw_pretty(img, thickness=1, output_path='./test_image_annotated.png')
 
 
